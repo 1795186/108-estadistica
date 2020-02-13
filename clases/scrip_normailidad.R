c5fb6213b9b187d3f4ab49181b28c81d51731bd5
1795186 <salva.mtz13@hotmail.com>
2020-02-13 09:20:07 -0600

    actualizar

--- clases/scrip_normailidad.R
+++ clases/scrip_normailidad.R
@@ -13,6 +13,70 @@ head(conjunto)
 # Aplicar prueba de shapiro.test para la ariable Altura -------------------
 
 shapiro.test(conjunto$Altura)
+
+shapiro.test(conjunto$Diametro)
+hist(conjunto$Diametro)
+
+
+
+boxplot(conjunto$Diametro ~ conjunto$Especie, col="red")
+boxplot(conjunto$Altura ~ conjunto$Especie, col="blue")
+boxplot(conjunto$Vecinos ~ conjunto$Especie, col="grey")
+frecu<-table(conjunto$Clase)
+prop.table(frecu)
+hist(frecu)
+boxplot(conjunto$Clase ~ conjunto$Especie, col="yellow")
+# prueba homogeniedad} ----------------------------------------------------
+
+var(conjunto$Diametro)
+var(conjunto$Altura)
+
+
+
+
+
+# datos de Ebanos ---------------------------------------------------------
+
+ebano <- read.csv("clases/ebanos.cSV", header = T)
+
+hist(ebano$diametro)
+hist(ebano$altura)
+
+boxplot(ebano$diametro)
+fivenum(ebano$diametro)
+
+
+# ¿cuantos individuos tienen  un Diam menoir al 1er quartil? --------------
+
+
+sum(ebano$diametro <=15.55)
+
+
+# ¿cuantos individuos tienen  un Diam menoir al 3er quartil? --------------
+
+sum(ebano$diametro <= 34.25)
+
+
+# ¿cuantos individuos tienen  un Diam mayor al 1er quartil? ---------------
+
+# pero menor o igual al 2do quartil? --------------------------------------
+
+sum(ebano$diametro >=15.55 & ebano$diametro <= 25.90)
+
+sum(ebano$diametro <=25.9)
+
+
+
+
+# ¿cuantos individuos tienen  un Diam menoir al 1er quartil? --------------
+
+  # pero menor o igual al 3er quartil? --------------------------------------
+
+sum(ebano$diametro >=10 & ebano$diametro <=15.55)
+
+
+
+
             
 ks.test(conjunto$Diametro, "pnorm",
         mean=mean(conjunto$Diametro,
